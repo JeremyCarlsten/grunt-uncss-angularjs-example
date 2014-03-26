6aedef0e82553795722a4c0f34bbb41114550563
jeremyCarlsten <jcc.993@gmail.com>
2014-03-26 18:20:02 -0500

    initial setup with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('jeremycarlsten.GruntUncssAngularExample', ['ngAnimate', 'ngRoute', 'mgcrea.ngStrap'])
+
+  .constant('version', 'v0.1.0')
+
+  .config(function($locationProvider, $routeProvider) {
+
+    $locationProvider.html5Mode(false);
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html'
+      })
+      .when('/features', {
+        templateUrl: 'views/features.html'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+  });
+
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('jeremycarlsten.GruntUncssAngularExample')
+
+  .controller('MainCtrl', function($scope, $location, version) {
+
+    $scope.$path = $location.path.bind($location);
+    $scope.version = version;
+
+  });
