06ca0b10aea3b338a0d5c1b5da4090be1c87aff9
JeremyCarlsten <jeremy@jeremycarlsten.com>
2015-04-28 08:53:49 -0500

    moving app.js to top level

--- app/scripts/controllers/main.js
+++
@@ -1,9 +0,0 @@
-//'use strict';
-//
-//angular.module('jeremycarlsten.GruntUncssAngularExample')
-//  .controller('MainCtrl', function($scope, $location, version) {
-//
-//    $scope.$path = $location.path.bind($location);
-//    $scope.version = version;
-//
-//  });
